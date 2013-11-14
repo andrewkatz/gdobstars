92a6e188c30d81fe4440be4ec9de4ec6d83b47ad
Andrew Katz <andrew.katz@outright.com>
2013-11-14 12:07:20 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gdobstars::Application.config.secret_key_base = '00a92faf3d776c0bcd8b4bfa71f57720ab516875be2061f4f0e4240eafeb72db7d9bf66439ea60602fd123ae3d1628e8f12d8b89be475884dfea1bd3d30a2d4e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gdobstars::Application.config.session_store :cookie_store, key: '_gdobstars_session'
